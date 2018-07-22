53388cebadca64369312300850841ed6a116a128
Pablo Benito González <pablobenitogonzalez@gmail.com>
2018-07-22 17:44:44 +0200

    added spring-data-rest with pagination interface

--- src/main/java/io/spring/garage/repositories/BicycleRepository.java
+++ src/main/java/io/spring/garage/repositories/BicycleRepository.java
@@ -1,7 +1,7 @@
 package io.spring.garage.repositories;
 
 import io.spring.garage.entities.vehicle.Bicycle;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface BicycleRepository extends CrudRepository<Bicycle, Long> {
+public interface BicycleRepository extends PagingAndSortingRepository<Bicycle, Long> {
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,25 @@
+insert into vehicle(id,color,model,num_wheels,type)values(1,'red','fiat',4,'CAR');
+insert into vehicle(id,color,model,num_wheels,type)values(2,'black','seat',4,'CAR');
+insert into vehicle(id,color,model,num_wheels,type)values(3,'black','seat',4,'CAR');
+insert into vehicle(id,color,model,num_wheels,type)values(4,'black','seat',4,'CAR');
+insert into vehicle(id,color,model,num_wheels,type)values(5,'red','seat',4,'CAR');
+insert into vehicle(id,color,model,num_wheels,type)values(6,'red','honda',2,'MOTORBIKE');
+insert into vehicle(id,color,model,num_wheels,type)values(7,'black','yamaha',2,'MOTORBIKE');
+insert into vehicle(id,color,model,num_wheels,type)values(8,'black','canyon',2,'BICYCLE');
+insert into vehicle(id,color,model,num_wheels,type)values(9,'black','blue',2,'BICYCLE');
+insert into vehicle(id,color,model,num_wheels,type)values(10,'red','rebel',2,'BICYCLE');
+insert into vehicle(id,color,model,num_wheels,type)values(11,'red','century',2,'BICYCLE');
+
+insert into car(id,plate)values(1,'KCS-45678');
+insert into car(id,plate)values(2,'ABA-12345');
+insert into car(id,plate)values(3,'ABA-22222');
+insert into car(id,plate)values(4,'ABA-00000');
+insert into car(id,plate)values(5,'ABA-99999');
+
+insert into motor_bike(id,plate)values(6,'KCS-99999');
+insert into motor_bike(id,plate)values(7,'ABA-77777');
+
+insert into bicycle(id)values(8);
+insert into bicycle(id)values(9);
+insert into bicycle(id)values(10);
+insert into bicycle(id)values(11);
