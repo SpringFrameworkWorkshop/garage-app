92a43cf1fe7bf09901c0c543aca7d9bb3ead76a6
Pablo Benito González <pablobenitogonzalez@gmail.com>
2018-07-28 12:30:06 +0200

    fix swagger

--- src/main/java/io/spring/garage/GarageApplication.java
+++ src/main/java/io/spring/garage/GarageApplication.java
@@ -10,9 +10,11 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
+@EnableSwagger2
 @SpringBootApplication
 public class GarageApplication {
 
