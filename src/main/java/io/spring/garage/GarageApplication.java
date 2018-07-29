8747e03932a9fc7762e708b0aad513ecb103cc85
Pablo Benito González <pablobenitogonzalez@gmail.com>
2018-07-29 13:03:41 +0200

    refactor swagger config

--- src/main/java/io/spring/garage/GarageApplication.java
+++ src/main/java/io/spring/garage/GarageApplication.java
@@ -4,17 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
-
-@EnableSwagger2
 @SpringBootApplication
 public class GarageApplication {
 
@@ -25,26 +15,6 @@ public class GarageApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
-    }
-
-    @Bean
-    public Docket swaggerSpringMvcPlugin() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("io.spring.garage.controllers"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(this.apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "Garage App REST API",
-                "Some custom description of API",
-                "API 1.0",
-                "Terms of service",
-                new Contact("John Doe", "www.example.com", "myeaddress@company.com"),
-                "License of API", "API license URL", Collections.emptyList());
     }
 
 }
